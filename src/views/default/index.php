93541ad529e60c44b1e1cd15a9b396e561e7f17f
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-06-07 09:26:45 +0000

    Updates index view

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -1,65 +1,76 @@
 <?php
-/**
- * @var View $this
- * @var SourceMessageSearch $searchModel
- * @var ActiveDataProvider $dataProvider
- */
-
-use yii\data\ActiveDataProvider;
 use yii\grid\GridView;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
-use yii\web\View;
 use yii\widgets\Breadcrumbs;
 use yii\widgets\Pjax;
-use Zelenin\yii\modules\I18n\models\search\SourceMessageSearch;
-use Zelenin\yii\modules\I18n\models\SourceMessage;
-use Zelenin\yii\modules\I18n\Module;
 
-$this->title = Module::t('Translations');
+/**
+ * @var \yii\web\View $this
+ * @var \greeflas\yii\modules\I18n\models\search\SourceMessageSearch $searchModel
+ * @var \yii\data\ActiveDataProvider $dataProvider
+ */
+
+$this->title = 'Переводы';
 echo Breadcrumbs::widget(['links' => [
     $this->title
 ]]);
 ?>
-<div class="message-index">
-    <h3><?= Html::encode($this->title) ?></h3>
-    <?php
-    Pjax::begin();
-    echo GridView::widget([
-        'filterModel' => $searchModel,
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            [
-                'attribute' => 'id',
-                'value' => function ($model, $index, $dataColumn) {
-                    return $model->id;
-                },
-                'filter' => false
-            ],
-            [
-                'attribute' => 'message',
-                'format' => 'raw',
-                'value' => function ($model, $index, $widget) {
-                    return Html::a($model->message, ['update', 'id' => $model->id], ['data' => ['pjax' => 0]]);
-                }
-            ],
-            [
-                'attribute' => 'category',
-                'value' => function ($model, $index, $dataColumn) {
-                    return $model->category;
-                },
-                'filter' => ArrayHelper::map($searchModel::getCategories(), 'category', 'category')
-            ],
-            [
-                'attribute' => 'status',
-                'value' => function ($model, $index, $widget) {
-                    /** @var SourceMessage $model */
-                    return $model->isTranslated() ? 'Translated' : 'Not translated';
-                },
-                'filter' => $searchModel->getStatus()
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title"><?= Html::encode($this->title) ?></h3>
+    </div>
+
+    <div class="panel-body">
+        <?php
+        Pjax::begin();
+        echo GridView::widget([
+            'filterModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+            'columns' => [
+                [
+                    'attribute' => 'id',
+                    'value' => function ($model, $index, $dataColumn) {
+                        return $model->id;
+                    },
+                    'filter' => false
+                ],
+                [
+                    'attribute' => 'message',
+                    'format' => 'raw',
+                    'value' => function ($model, $index, $widget) {
+                        return Html::a($model->message, ['update', 'id' => $model->id], ['data' => ['pjax' => 0]]);
+                    }
+                ],
+                [
+                    'attribute' => 'translation',
+                    'format' => 'raw',
+                    'value' => function ($model, $index, $widget) {
+                        return $model->getDefaultLangTranslation();
+                    }
+                ],
+                [
+                    'attribute' => 'category',
+                    'value' => function ($model, $index, $dataColumn) {
+                        return $model->category;
+                    },
+                    'filter' => ArrayHelper::map($searchModel::getCategories(), 'category', 'category')
+                ],
+                [
+                    'attribute' => 'status',
+                    'value' => function ($model, $index, $widget) {
+                        return \Zelenin\yii\modules\I18n\models\Message::isModelFullyTranslated($model->id)
+                            ? 'Переведено'
+                            : 'Не переведено';
+                    },
+                    'filter' => Html::dropDownList($searchModel->formName() . '[status]', $searchModel->status, $searchModel->getStatus(), [
+                        'class' => 'form-control',
+                        'prompt' => ''
+                    ])
+                ]
             ]
-        ]
-    ]);
-    Pjax::end();
-    ?>
+        ]);
+        Pjax::end();
+        ?>
+    </div>
 </div>
